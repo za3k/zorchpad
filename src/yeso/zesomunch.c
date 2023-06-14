670a034eb872b51d1e2ef64bb912f7b3f2101f04
Zachary Vance <za3k@za3k.com>
2023-06-14 02:08:37 -0400

    correct munch

--- src/yeso/zesomunch.c
+++ src/yeso/zesomunch.c
@@ -6,13 +6,12 @@
 int main()
 {
   zwin w = zw_open();
-  for (int t = 0;; t++) {
+  for (int t = 0;; t = (t+1)%512) {
     for (int y = 0; y < HEIGHT_PX; y++) {
       int x = t ^ y;
       zpix cur = zw_get_pixel(w, x, y);
       zw_set_pixel(w, x, y, cur ? 0 : 1);
     }
     zw_flip(w);
-    sleep(0.1);
   }
 }
