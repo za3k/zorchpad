c3fe1c518539975607e51f2c8a8226fa91cca221
Zachary Vance <za3k@za3k.com>
2023-06-16 16:52:05 -0400

    add font test to hello world

--- src/yeso/helloworld.c
+++ src/yeso/helloworld.c
@@ -9,6 +9,11 @@ int main()
   char *msg = "Hello, world!";
   while (*msg!='\0') {
     zw_set_char(w, x++, y, *msg++, 0);
+    zw_flip(w);
+  }
+  for (int c=0; c<=255; c++) {
+    zw_set_char(w, c%50, (c/50)+1, c, 0);
+    zw_flip(w);
   }
   zw_flip(w);
   while (1) zw_flip(w);
